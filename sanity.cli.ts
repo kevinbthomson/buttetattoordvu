8022a294d2afc43379acfea1478565fc21c69820
Kevin Thomson <kevinthomson@Kevins-MacBook.local>
2026-04-03 18:01:46 -0600

    fix: update sanity config

---
+++ sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from 'sanity/cli';
+
+export default defineCliConfig({
+  api: {
+    projectId: 'f70v9lhe',
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 'y9mw12s451zh7hu2k9zuk5k6',
+  },
+});
